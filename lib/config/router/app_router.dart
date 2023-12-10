ed7e7fc609eb8242e0f97cea7b51a6debd4ad1eb
Damian Sanchez <sanchezdamianj@gmail.com>
2023-12-10 15:28:31 -0300

    shellRoutes by go_router documentation

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,18 +1,51 @@
+import 'package:cuevanax/presentations/views/views.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cuevanax/presentations/screens/screens.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-      routes: [
-        GoRoute(
+  ShellRoute(
+    builder: (context, state, child) {
+      return HomeScreen(childView: child);
+    },
+    routes: [
+      GoRoute(
+        path:'/',
+        builder: (context, state) {
+          return const HomeView();
+        },
+        routes: [
+          GoRoute(
             path: 'movie/:id',
-            name: MovieScreen.name,
             builder: (context, state) {
               final movieId = state.pathParameters['id'] ?? 'no-id';
               return MovieScreen(movieId: movieId);
-            }),
-      ]),
+            }
+          )
+        ]
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        ),
+
+      
+    ]
+  )
+
+//Routes Father/Son
+  // GoRoute(
+  //     path: '/',
+  //     name: HomeScreen.name,
+  //     builder: (context, state) => const HomeScreen(childView: HomeView()),
+  //     routes: [
+  //       GoRoute(
+  //           path: 'movie/:id',
+  //           name: MovieScreen.name,
+  //           builder: (context, state) {
+  //             final movieId = state.pathParameters['id'] ?? 'no-id';
+  //             return MovieScreen(movieId: movieId);
+  //           }),
+  //     ]),
 ]);
--- lib/presentations/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentations/widgets/shared/custom_bottom_navigation.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
 
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
 
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.toString();
+    switch (location) {
+      case '/':
+       return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped( BuildContext context, int index){
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/categories');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+      default:
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(elevation: 0, items: const [
+    return BottomNavigationBar(
+      currentIndex: getCurrentIndex(context),
+      elevation: 0,
+      onTap: (value) {
+        onItemTapped(context, value);
+      },
+      items: const [
       BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
       BottomNavigationBarItem(
           icon: Icon(Icons.label_outline), label: 'Categories'),
